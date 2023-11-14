ae322287d70eec73b930cff8b647c6c1f8de170c
jsalazar8 <jsalazar8@hawk.iit.edu>
2023-11-14 15:05:36 -0600

    corrected indentation

--- js/script.js
+++ js/script.js
@@ -2,20 +2,20 @@ function init() {
     var el = document.getElementById('mapcanvas');
     var myLocation = new google.maps.LatLng(20.682027, -103.462431);
     var mapOptions = {
-		center: myLocation,
-		zoom: 15,
-		mapTypeId: google.maps.MapTypeId.SATELLITE,
-		mapTypeControlOptions: {
-			position: google.maps.ControlPosition.BOTTOM_CENTER
+	  center: myLocation,
+	  zoom: 15,
+	  mapTypeId: google.maps.MapTypeId.SATELLITE,
+	  mapTypeControlOptions: {
+	  position: google.maps.ControlPosition.BOTTOM_CENTER
 		}
 	};
 
 	var myMap = new google.maps.Map(el, mapOptions);
     var marker = new google.maps.Marker({
-		position: myLocation,
-		map: myMap,
-		animation: google.maps.Animation.DROP,
-        icon:'images/chivas.png'
+	  position: myLocation,
+	  map: myMap,
+      animation: google.maps.Animation.DROP,
+      icon:'images/chivas.png'
     });
 
 
